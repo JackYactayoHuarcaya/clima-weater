5713ed3610662ba54a0947431fd056011895932a
jack <jack-0204@hotmail.com>
2025-05-04 17:59:27 -0500

    avance1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,22 @@
 import WeatherWidget from "./components/Weather";
 import imgLogo from "../assets/jack-svg.svg";
 import { Github } from "./components/Github";
+
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Solución limpia y segura sin usar `any`
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl: unknown })
+  ._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const App = () => {
   return (
     <div className="w-full h-screen bg-gray-800">
